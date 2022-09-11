55ce3306b6c6306d4e0c305dde8186e0f8d536f1
Priscila Leite <pricila135leite@gmail.com>
2022-09-11 14:54:28 -0300

    settings admin.py
    settings admin.py with categories end pages in app_rango.

--- app_rango/admin.py
+++ app_rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from rango.models import category, page
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+#Personalizando a classe para preenchimento automatico do 'Slug' ao digitar a category.
+class categoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)} 
+
+#Atualiza o registro incluindo 'categoryAdmin' para incluir a classe persolizada
+admin.site.register(category, categoryAdmin)
+admin.site.register(page, PageAdmin)
